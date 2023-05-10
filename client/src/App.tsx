caa3d86a408ec6cf78b7c43d33078939ed1ec0dd
delbaze <56109313+delbaze@users.noreply.github.com>
2023-05-10 15:54:46 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,8 +9,10 @@ function App() {
       <NavLink to={"/login"}>Se connecter</NavLink>
       <NavLink to={"/books"}>Liste des livres</NavLink>
       <NavLink to={"/logout"}>Se déconnecter</NavLink>
-      {Object.keys(userInfos).length > 0 && (
-        <p>connecté en tant que {userInfos?.email}</p>
+      {Object.keys(userInfos).length > 0 ? (
+        <p>Vous êtes connectés en tant que {userInfos?.email}</p>
+      ) : (
+        <p>Vous n'êtes pas connectés</p>
       )}
       <Outlet />
     </div>
